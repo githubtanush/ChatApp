e2cfd984b53cffc126f20bcd861d901a48ae7fe3
Tanush Arora <tanush935@gmail.com>
2025-07-05 20:05:26 +0530

    loginpage2

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -8,6 +8,13 @@ const LoginPage = () => {
   const [password,setPassword] = useState("")
   const [bio, setBio] = useState("")
   const [isDataSubmitted,setIsDataSubmitted] = useState(false);
+  const onSubmitHandler = (event) =>{
+    event.preventDefault();
+    if(currState === 'Sign up' && !isDataSubmitted){
+      setIsDataSubmitted(true)
+      return ;
+    }
+  }
 
   return (
     <div className='min-h-screen bg-cover bg-center flex items-center justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blue-2xl bg-[url("/bgImage.svg")]'>
@@ -15,10 +22,16 @@ const LoginPage = () => {
       <img src={assets.logo_big} alt="logo" className='w-[min(30vw,250px)]'/>
 
       {/* ------ right ----- */}
-     <form className='border-2 bg-white/10 text-white border-gray-500 p-6 flex flex-col gap-6 rounded-lg shadow-lg'>
+     <form 
+     onSubmit={onSubmitHandler}
+      className='border-2 bg-white/10 text-white border-gray-500 p-6 flex flex-col gap-6 rounded-lg shadow-lg'>
      <h2 className='font-medium text-2xl flex justify-between items-center'>
       {currState}
-      <img src={assets.arrow_icon} alt="" className='w-5 cursor-pointer'/>
+      {isDataSubmitted && 
+      <img
+      onClick={()=>setIsDataSubmitted(false)} 
+      src={assets.arrow_icon} alt="" className='w-5 cursor-pointer'/>
+      }
       </h2>
       
       {currState === "Sign up" && !isDataSubmitted && (
