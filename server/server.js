cdbcb4ff9f87c0f395c6694b001c6c1fa627e85c
Tanush Arora <tanush935@gmail.com>
2025-07-16 13:15:35 +0530

    message controller commit

--- server/server.js
+++ server/server.js
@@ -3,16 +3,40 @@ import "dotenv/config";
 import cors from "cors";
 import http from "http";
 import { connectDB } from "./lib/db.js";
+import userRouter from "./routes/userroutes.js"
+import messageRouter from "./routes/messageroutes.js";
+import { Server } from "socket.io";
 
 //create Express app and HTTP server
 const app = express();
 const server = http.createServer(app);
 
+//Initialize socket.io server
+export const io = new Server(server,{
+    cors: {origin : "*"}
+})
+
+//Store online users
+export const userSocketMap = {};
+//socket.io connection handler
+io.on("connection",(socket)=>{
+    const userId = socket.handshake.query.userId;
+    console.log("User Connected", userId);
+
+    if(userId) userSocketMap[userId] = socket.id;
+    //Emit online users to all connected client
+    io.emit("getOnlineUsers",Object.keys())
+})
+
 //Middleware setup
 app.use(express.json({limit: "4mb"}));
 app.use(cors());
 
-app.use("/api/status", (req,res)=>res.send("Server is live"))
+//Routes setup
+app.use("/api/status", (req,res)=>res.send("Server is live"));
+app.use("/api/auth",userRouter);
+app.use("/api/messages",messageRouter);
+
 
 //connect to MongoDB
 await connectDB();
